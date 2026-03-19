6e621092fb367f567bc33c1a07d1b69e4aa456a5
“Anushka <anushka.kufam22@sinhgad.edu>
2026-03-20 01:12:10 +0530

    all questions of assignment

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -1,9 +1,15 @@
+import java.util.Arrays;
+
 class ArrayAssignment{
     public static void main(String[] args) {
         /*int[] arr ={1,3,5,7};
-        System.out.println(linearSearch(arr, 3));*/
+        System.out.println(linearSearch(arr, 3));
         int[] arr={1,2,2,3,4,4};
-        System.out.println(removeDuplicates(arr));
+        System.out.println(removeDuplicates(arr));*/
+        int[] arr={1,2,0,0,3,0,4};
+        int[] result =(moveZerosToEnd(arr));
+        System.out.println(Arrays.toString(result));
+
     }
 
     static int[] removeDuplicates(int[] arr){
@@ -21,8 +27,20 @@ class ArrayAssignment{
     }
 
     static int[] moveZerosToEnd(int[] arr){
-        
+        int i=0;
+        for(int j=0 ; j < arr.length ; j++){
+        if(arr[j]!=0){
+            arr[i]=arr[j];
+            i++;
+        }
     }
+    while (i < arr.length) {
+        arr[i] = 0;
+        i++;
+    }
+    return arr;
+}
+
 
     static int linearSearch(int[] arr , int target){
         for(int i=0;i<arr.length;i++){
