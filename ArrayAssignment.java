c706086df12479a39366e471598a0739f2624a32
“Anushka <anushka.kufam22@sinhgad.edu>
2026-03-20 00:46:29 +0530

    remove duplicates for sorted array

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -2,17 +2,26 @@ class ArrayAssignment{
     public static void main(String[] args) {
         /*int[] arr ={1,3,5,7};
         System.out.println(linearSearch(arr, 3));*/
+        int[] arr={1,2,2,3,4,4};
+        System.out.println(removeDuplicates(arr));
     }
 
-    static int removeDuplicates(){
-
+    static int[] removeDuplicates(int[] arr){
+        //BUT THIS IS ONLY FOR SORETED ARRAY 
+        //{1,1,3,3,4,5} ->> {1,3,4,5};
+        if(arr.length == 0) return 0;
+        int i=0;
+        for(int j=0;j<arr.length;j++){
+            if(arr[i]!=arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+            return i+1;
     }
 
     static int[] moveZerosToEnd(int[] arr){
-        //XOR 
-        for(int i=0;i<arr.length;i++){
-            arr[i]^arr[1+1];
-        }
+        
     }
 
     static int linearSearch(int[] arr , int target){
